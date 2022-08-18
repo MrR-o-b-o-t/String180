964b50d5da290b0d6467b54a353149ce2a1acf26
Sam Kautz <50638220+Web-Dev-Jr@users.noreply.github.com>
2022-08-18 15:13:19 -0500

    Init Commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,27 @@
-// Call helloWorld()
-function helloWorld() {
-    alert("Hello World");
+//Get the string from page
+function getValue() {
+    document.getElementById("alert").classList.add("invisible")
+    let userString = document.getElementById("userString").value
+    let reversedString = reverseString(userString)
+    displayString(reversedString)
+}
+
+
+//Reverse the string
+
+function reverseString(userString) {
+    let reversedString = []
+    for (let i = userString.length -1; i >= 0; i--) {
+        reversedString += userString[i]
+    }
+
+    return reversedString
+}
+
+
+//Display message and string
+
+function displayString(reversedString) {
+document.getElementById("msg").innerHTML = `Your string reversed is: ${reversedString}`
+document.getElementById("alert").classList.remove("invisible")
 }
